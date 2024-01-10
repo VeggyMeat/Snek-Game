40a2b1f414d63aa81b617f38ce563652ead24ce5
VeggyMeat <67714188+VeggyMeat@users.noreply.github.com>
2024-01-10 12:24:32 +0000

    Working V-0.17.1
    -Made all of the BodyTriggers in TriggerManager read only

--- Assets/Classes/Passives/TriggerManager.cs
+++ Assets/Classes/Passives/TriggerManager.cs
@@ -6,33 +6,33 @@ using UnityEngine;
 
 public static class TriggerManager
 {
-    public static BodyTrigger<(BodyController, int)> BodyLostHealthTrigger = new BodyTrigger<(BodyController, int)>();
+    public readonly static BodyTrigger<(BodyController, int)> BodyLostHealthTrigger = new BodyTrigger<(BodyController, int)>();
 
-    public static BodyTrigger<(BodyController, int)> BodyGainedHealthTrigger = new BodyTrigger<(BodyController, int)>();
+    public readonly static BodyTrigger<(BodyController, int)> BodyGainedHealthTrigger = new BodyTrigger<(BodyController, int)>();
 
-    public static BodyTrigger<int> BodyLevelUpTrigger = new BodyTrigger<int>();
+    public readonly static BodyTrigger<int> BodyLevelUpTrigger = new BodyTrigger<int>();
 
-    public static BodyTrigger<BodyController> BodyDeadTrigger = new BodyTrigger<BodyController>();
+    public readonly static BodyTrigger<BodyController> BodyDeadTrigger = new BodyTrigger<BodyController>();
 
-    public static BodyTrigger<BodyController> BodySpawnTrigger = new BodyTrigger<BodyController>();
+    public readonly static BodyTrigger<BodyController> BodySpawnTrigger = new BodyTrigger<BodyController>();
 
-    public static BodyTrigger<GameObject> EnemySpawnTrigger = new BodyTrigger<GameObject>();
+    public readonly static BodyTrigger<GameObject> EnemySpawnTrigger = new BodyTrigger<GameObject>();
 
-    public static BodyTrigger<GameObject> EnemyDeadTrigger = new BodyTrigger<GameObject>();
+    public readonly static BodyTrigger<GameObject> EnemyDeadTrigger = new BodyTrigger<GameObject>();
 
-    public static BodyTrigger<EnemyController> EnemyLostHealthTrigger = new BodyTrigger<EnemyController>();
+    public readonly static BodyTrigger<EnemyController> EnemyLostHealthTrigger = new BodyTrigger<EnemyController>();
 
-    public static BodyTrigger<GameObject> ProjectileShotTrigger = new BodyTrigger<GameObject>();
+    public readonly static BodyTrigger<GameObject> ProjectileShotTrigger = new BodyTrigger<GameObject>();
 
-    public static BodyTrigger<BodyController> BodyRevivedTrigger = new BodyTrigger<BodyController>();
+    public readonly static BodyTrigger<BodyController> BodyRevivedTrigger = new BodyTrigger<BodyController>();
 
-    public static BodyTrigger<int> StartTurningTrigger = new BodyTrigger<int>();
+    public readonly static BodyTrigger<int> StartTurningTrigger = new BodyTrigger<int>();
 
-    public static BodyTrigger<int> StopTurningTrigger = new BodyTrigger<int>();
+    public readonly static BodyTrigger<int> StopTurningTrigger = new BodyTrigger<int>();
 
-    public static BodyTrigger<int> PreBodyMoveTrigger = new BodyTrigger<int>();
+    public readonly static BodyTrigger<int> PreBodyMoveTrigger = new BodyTrigger<int>();
 
-    public static BodyTrigger<int> PostBodyMoveTrigger = new BodyTrigger<int>();
+    public readonly static BodyTrigger<int> PostBodyMoveTrigger = new BodyTrigger<int>();
 
-    public static BodyTrigger<GameObject> ProjectileHitTrigger = new BodyTrigger<GameObject>();
+    public readonly static BodyTrigger<GameObject> ProjectileHitTrigger = new BodyTrigger<GameObject>();
 }
